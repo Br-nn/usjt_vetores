977150af21cba79daa66534e097e440936656726
Bruno Dias <brunogd148@gmail.com>
2023-10-24 21:39:21 -0300

    ap07(vetores): ordem alfabetica e remoção add

--- MenuMusica.java
+++ MenuMusica.java
@@ -6,7 +6,7 @@ public class MenuMusica {
     public static void main(String[] args) {
        List <Musica> playlist = new ArrayList<>(0);
        int op;
-       String menu = "1-Adicionar\n2-Ver a playlist\n0-Sair";
+       String menu = "1-Adicionar\n2-Ver a playlist\n3- Remover Música\n0-Sair";
        do{
         op = parseInt(showInputDialog(menu));
         switch(op){
@@ -18,6 +18,7 @@ public class MenuMusica {
             }
             case 2:{
                 StringBuilder sb = new StringBuilder("");
+                Collections.sort(playlist);
                 for(Musica musica : playlist){
                     sb.append(musica.getTitulo());
                     sb.append("\n");
@@ -25,6 +26,11 @@ public class MenuMusica {
               }
               showMessageDialog(null, sb);
               break;
+            }
+            case 3:{
+                int indice = parseInt(showInputDialog(null, "Digite a posição da música a ser removida"));
+                playlist.remove(indice - 1);
+                showMessageDialog(null, "Música removida");
             }
         }
        }while (op != 0); 
--- Musica.java
+++ Musica.java
@@ -1,10 +1,13 @@
 
-public class Musica {
+public class Musica implements Comparable <Musica>{
     private String titulo;
     private int avaliacao;
 
     public Musica(String titulo){
        this.titulo = titulo; 
+    }
+    public int compareTo(Musica m){
+        return this.getTitulo().compareTo(m.getTitulo());
     }
     public void setAvaliacao(int avaliacao) {
         this.avaliacao = avaliacao;
